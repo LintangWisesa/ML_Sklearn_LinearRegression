b16a57749445a1d6bd852b9a6b6ca4dc2ded11a5
lintangwisesa <lintangwisesa@ymail.com>
2019-02-28 11:55:24 +0700

    linear regression

---
+++ 3_hargaTanah.py
@@ -0,0 +1,49 @@
+# pip install sklearn
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# ================================
+# create dataframe
+
+df = pd.read_csv('3_hargaTanah.csv')
+print(df)
+
+# ================================
+# linear regression
+
+from sklearn import linear_model
+model = linear_model.LinearRegression()
+
+# training w/ dataset: mode.fit(dataIndependent[2D], dataDependent[1D])
+model.fit(df[['luas']], df['harga'])
+
+# show m(gradient/slope) & b(intercept) of 'best fit line'
+print('Slope = ', model.coef_)
+print('Intercept = ', model.intercept_)
+
+# ================================
+
+# # plot dataframe
+plt.plot(df['luas'], df['harga'], 'r-')
+plt.scatter(df['luas'], df['harga'], color='r', marker='o', s=50)
+
+# # plot best fit line
+plt.plot(df['luas'], model.predict(df[['luas']]), 'g-')
+plt.scatter(df['luas'], model.predict(df[['luas']]), color='g', marker='*', s=50)
+
+plt.xlabel('Nilai X')
+plt.ylabel('Nilai Y')
+plt.title('Linear Regression')
+plt.show()
+
+# =========================
+
+# prediction harga tanah saya
+tanahSaya = {
+    'luas': [500, 1234, 4200, 2345]
+}
+dfTanahSaya = pd.DataFrame(tanahSaya)
+print(model.predict(dfTanahSaya))
